9bd5eb603979e3de1d60e1f98e362373e6ed3f33
travi4ka <sergey.zhiharev@mbicycle.com>
2023-02-05 12:26:49 +0300

    remote + allure

--- src/test/java/tests/TestsBase.java
+++ src/test/java/tests/TestsBase.java
@@ -1,9 +1,14 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.AutomationPracticeFormPage;
+import io.qameta.allure.selenide.AllureSelenide;
 
 import java.util.Map;
 
@@ -24,5 +29,18 @@ public class TestsBase {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
